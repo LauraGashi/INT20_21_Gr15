cb61fe34b68334c035a62b7f5a257a291edf1fcd
Alma-L <75323781+Alma-L@users.noreply.github.com>
2021-01-15 19:15:54 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const worker = new Worker('worker.js')
+worker.addEventListener('message',({data})=>{
+    if(data.action =='countTo2Billion'){
+        console.log('We re done without blocking the UI thread')
+    }
+})
+
+
+
+const now =performance.now();
+
+worker.postMessage({type:'cmd',action:'countTo2Billion'})
+console.log('Time taken is ${performance.now()-now}')
+
+
+
+alert("Something on UI thread");
